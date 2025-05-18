aa2c39eb537f971d3106fdabe95c1224584be581
ruymon <ruystfrancis@gmail.com>
2025-05-17 23:25:56 -0300

    feat: dashboard page

--- src/views/app/AppLayoutView.vue
+++ src/views/app/AppLayoutView.vue
@@ -5,10 +5,10 @@ import NavItem from '@/components/NavItem.vue'
 
 <template>
   <div class="h-svh flex bg-zinc-100">
-    <nav class="h-full bg-white w-24 flex flex-col justify-between items-center pt-8">
+    <aside class="h-full bg-white w-24 flex flex-col justify-between items-center pt-8">
       <img class="w-12" alt="Move.it icon" src="@/assets/icon.svg" />
 
-      <div class="flex flex-col gap-4 -translate-y-12 grow justify-center w-full">
+      <nav class="flex flex-col gap-4 -translate-y-12 grow justify-center w-full">
         <NavItem to="/app">
           <PhHouse :size="32" />
         </NavItem>
@@ -16,8 +16,8 @@ import NavItem from '@/components/NavItem.vue'
         <NavItem to="/app/leaderboard">
           <PhRanking :size="32" />
         </NavItem>
-      </div>
-    </nav>
+      </nav>
+    </aside>
 
     <div class="flex-1 flex mx-auto max-w-6xl px-8 pt-10 overflow-hidden">
       <RouterView />
--- src/views/app/DashboardView.vue
+++ src/views/app/DashboardView.vue
@@ -1,5 +1,74 @@
+<script setup lang="ts">
+import { PhArrowFatLinesUp, PhPlay } from '@phosphor-icons/vue'
+</script>
+
 <template>
-  <main class="flex flex-col flex-1">
-    <h1>App home</h1>
+  <main class="flex flex-col flex-1 gap-24">
+    <div class="w-full flex items-center">
+      <span class="w-20 text-sm text-zinc-400">0 xp</span>
+
+      <div class="flex-1 h-1.5 rounded-full bg-zinc-200 overflow-clip">
+        <div class="w-[0%] bg-green-500 h-full" />
+      </div>
+
+      <span class="w-20 text-sm text-zinc-400 text-right">600 xp</span>
+    </div>
+
+    <main class="flex gap-32">
+      <aside class="w-full flex flex-col gap-14">
+        <header class="flex items-center gap-4">
+          <img class="w-14 rounded-full" alt="User avatar" src="https://github.com/ruymon.png" />
+
+          <div class="flex flex-col">
+            <span class="text-2xl font-semibold text-zinc-700">Ruy Monteiro</span>
+            <span class="text-zinc-500">level 1</span>
+          </div>
+        </header>
+
+        <div
+          class="flex text-lg text-zinc-500 items-center justify-between border-b border-zinc-200 pb-4"
+        >
+          <span>Desafios completados</span>
+          <span class="font-medium">00</span>
+        </div>
+
+        <div
+          class="flex h-36 w-full justify-between text-zinc-700 font-semibold text-9xl leading-0"
+        >
+          <div class="overflow-clip rounded-2xl flex gap-1 h-full">
+            <span class="bg-white w-28 inline-flex justify-center items-center">3</span>
+            <span class="bg-white w-28 inline-flex justify-center items-center">5</span>
+          </div>
+          <div class="flex flex-col justify-center gap-4 px-4">
+            <div class="min-h-3 aspect-square bg-zinc-700 rounded-full" />
+            <div class="min-h-3 aspect-square bg-zinc-700 rounded-full" />
+          </div>
+          <div class="overflow-clip rounded-2xl flex gap-1 h-full">
+            <span class="bg-white w-28 inline-flex justify-center items-center">0</span>
+            <span class="bg-white w-28 inline-flex justify-center items-center">0</span>
+          </div>
+        </div>
+
+        <button
+          class="bg-indigo-500 hover:cursor-pointer hover:bg-indigo-600 transition-all h-20 justify-center text-white flex items-center gap-2 py-4 px-6 rounded-xl"
+        >
+          <span class="font-semibold text-lg">Iniciar um ciclo</span>
+          <PhPlay :size="16" weight="fill" />
+        </button>
+      </aside>
+
+      <aside
+        class="w-full max-w-1/2 flex gap-16 p-16 flex-col items-center justify-center bg-white rounded-xl"
+      >
+        <h2 class="font-medium text-zinc-500 text-2xl text-center max-w-4/6">
+          Inicie um ciclo para receber desafios
+        </h2>
+
+        <div class="flex flex-col gap-4 items-center max-w-4/6 text-center">
+          <PhArrowFatLinesUp :size="80" weight="fill" class="text-green-500" />
+          <span class="text-zinc-400">Avance de level completando os desafios.</span>
+        </div>
+      </aside>
+    </main>
   </main>
 </template>
